fa6f7a770fd6bf226aae913e98011a70e3ede033
Mehdi Mirzapour <mehdi.mirzapour@gmail.com>
2025-07-03 03:29:14 +0200

    cleaning test cases

--- test/local_test.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("http://localhost:5173")
-    page.wait_for_timeout(5 * 1000)  # 5 minutes = 300000 ms
-
